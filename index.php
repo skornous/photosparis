4f3a9ca874f61bac8c7a806b5d466e58ca6a8739
ivykloud <ivykloud@gmail.com>
2015-04-08 18:41:23 +0200

    Creating Index.php o/

---
+++ index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Photos Paris</title>
+        <meta charset="UTF-8" />
+        <meta name="description" content="Concours Photos Paris" />
+
+    </head>
+    <body>
+        <h1>Bonjour !</h1>
+    </body>
+</html>
